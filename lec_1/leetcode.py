d997f960642ad12831af4d4f32646bba1bf4d328
Sakshi262005 <sakshipatel262005@gmail.com>
2024-06-25 23:10:54 +0530

    Delete lec_1/leetcode.py

--- lec_1/leetcode.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        d = {}
-        for i, num in enumerate(nums):
-            t = target - num
-            if t in d:
-                return [d[t], i]
-            d[num] = i
-        return [] 
