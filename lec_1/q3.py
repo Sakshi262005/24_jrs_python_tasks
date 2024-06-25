f605bdc19a5ab374482aa1ce9d6cd236155f0625
Sakshi262005 <sakshipatel262005@gmail.com>
2024-06-25 23:11:49 +0530

    Delete lec_1/q3.py

--- lec_1/q3.py
+++
@@ -1,9 +0,0 @@
-Computers ={
-  "Laptop1" : {"brand" : "DELL","OS" : "Windows"},
-  "Laptop2" : {"brand" : "HP", "OS" : "Linux"},
-  "Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
-}
-brand=[Computers["Laptop1"]["brand"],Computers["Laptop2"]["brand"],Computers["Desktop"]["brand"]]
-OS=[Computers["Laptop1"]["OS"],Computers["Laptop2"]["OS"],Computers["Desktop"]["OS"]]
-print(brand)
-print(OS)
